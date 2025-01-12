10ae3c7c21ecef35a59e8997a5e0984e570e4dce
MEDOYED <mmax26072005@gmail.com>
2025-01-12 14:33:46 +0100

    bug fixed with 401 error in MarvelServise

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -1,10 +1,17 @@
+import { MD5 } from "crypto-js";
+
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
-  _apiKey = "apikey=09f4bbc1578fd314c23453247423742f";
-  //   _apiKey = "apikey=869893b5876b37169290858bdddf33b7b3961e1d";
 
-  // https://gateway.marvel.com:443/v1/public/characters?apikey=09f4bbc1578fd314c23453247423742f
+  _publicKey = "09f4bbc1578fd314c23453247423742f";
+  _privateKey = "869893b5876b37169290858bdddf33b7b3961e1d";
+
+  _generateHash = () => {
+    const ts = new Date().getTime();
+    const hash = MD5(ts + this._privateKey + this._publicKey).toString();
+    return { ts, hash };
+  };
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -17,11 +24,16 @@ class MarvelService {
   };
 
   getAllCharacters = () => {
-    return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    const { ts, hash } = this._generateHash();
+    return this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&ts=${ts}&apikey=${this._publicKey}&hash=${hash}`
+    );
   };
 
   getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this.getResource(
+      `${this._apiBase}characters/${id}?ts=${ts}&apikey=${this._publicKey}&hash=${hash}`
+    );
   };
 }
 
